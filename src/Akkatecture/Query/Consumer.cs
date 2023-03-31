b26eb3d31387fd1f4d9b73b847bcb4b27b2252f0
David Cumps <david@cumps.be>
2023-03-31 23:01:56 +0200

    fix: use correct journal query in Consumer.CurrentEventsFromAggregate

--- src/Akkatecture/Query/Consumer.cs
+++ src/Akkatecture/Query/Consumer.cs
@@ -1,3 +1,30 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015-2021 Rasmus Mikkelsen
+// Copyright (c) 2015-2021 eBay Software Foundation
+//     Modified from original source https://github.com/eventflow/EventFlow
+// Copyright (c) 2018 - 2021 Lutando Ngqakaza
+//     https://github.com/Lutando/Akkatecture
+// Copyright (c) 2022 AfterLutz Contributors
+//     https://github.com/AfterLutz/Akketecture
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 using System.Linq;
 using Akka;
 using Akka.Actor;
@@ -13,7 +40,9 @@ namespace Akkatecture.Query
     public class Consumer
     {
         public ActorSystem ActorSystem { get; set; }
+
         protected string Name { get; set; }
+
         internal Consumer(
             string name,
             ActorSystem actorSystem)
@@ -32,14 +61,10 @@ namespace Akkatecture.Query
         }
 
         public static Consumer Create(string name, Config config)
-        {
-            return new Consumer(name,config);
-        }
-        
+            => new(name,config);
+
         public static Consumer Create(ActorSystem actorSystem)
-        {
-            return new Consumer(actorSystem.Name, actorSystem);
-        }
+            => new(actorSystem.Name, actorSystem);
 
         public Consumer<TJournal> Using<TJournal>(
             string readJournalPluginId = null)
@@ -48,48 +73,46 @@ namespace Akkatecture.Query
             var readJournal = PersistenceQuery
                 .Get(ActorSystem)
                 .ReadJournalFor<TJournal>(readJournalPluginId);
-            
+
             return new Consumer<TJournal>(Name, ActorSystem, readJournal);
         }
     }
-    
+
     public class Consumer<TJournal> : Consumer
         where TJournal : IEventsByTagQuery, ICurrentEventsByTagQuery
     {
         protected TJournal Journal { get; }
-        
-        public Consumer(
+
+        internal Consumer(
             string name,
             ActorSystem actorSystem,
-            TJournal journal) 
+            TJournal journal)
             : base(name, actorSystem)
-        {
-            Journal = journal;
-        }
+            => Journal = journal;
 
         public Source<EventEnvelope, NotUsed> EventsFromAggregate<TAggregate>(Offset offset = null)
             where TAggregate : IAggregateRoot
         {
             var mapper = new DomainEventReadAdapter();
             var aggregateName = typeof(TAggregate).GetAggregateName();
-            
+
             return Journal
-                .EventsByTag(aggregateName.Value, offset)
+                .EventsByTag(aggregateName.Value, offset ?? Offset.NoOffset())
                 .Select(x =>
                 {
                     var domainEvent = mapper.FromJournal(x.Event, string.Empty).Events.Single();
                     return new EventEnvelope(x.Offset, x.PersistenceId, x.SequenceNr, domainEvent, x.Timestamp);
                 });
         }
-        
+
         public Source<EventEnvelope, NotUsed> CurrentEventsFromAggregate<TAggregate>(Offset offset = null)
             where TAggregate : IAggregateRoot
         {
             var mapper = new DomainEventReadAdapter();
             var aggregateName = typeof(TAggregate).GetAggregateName();
-            
+
             return Journal
-                .EventsByTag(aggregateName.Value, offset)
+                .CurrentEventsByTag(aggregateName.Value, offset ?? Offset.NoOffset())
                 .Select(x =>
                 {
                     var domainEvent = mapper.FromJournal(x.Event, string.Empty).Events.Single();
@@ -97,6 +120,4 @@ namespace Akkatecture.Query
                 });
         }
     }
-    
-    
 }
